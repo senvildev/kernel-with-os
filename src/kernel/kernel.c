ea943fa4b2d36e43c93b033b3354d9a010c0075d
senvildev <realsenvil@gmail.com>
2025-03-04 20:39:28 +0100

    updated the kernel main file

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -2,13 +2,12 @@
 #include <stdint.h>
 
 #include "tty/tty.h"
+#include "gdt/gdt.h"
 
-static inline uint8_t inb(uint16_t port)
-{
-	uint8_t result;
-	__asm__ volatile("inb %1, %0" : "=a"(result) : "Nd"(port));
-	return result;
-}
+#include "libs/kernel_log/kernel_log.h"
+#include "libs/stdout/stdout.h"
+
+// #include "input/keyboard/keyboard.h"
 
 const size_t CODES_PRESSED_SIZE = 59;
 const char CODES_PRESSED[59] = {
@@ -24,64 +23,9 @@ void kernel_main(void)
 {
 	tty_initialize();
 
-	uint8_t last_scancode;
+	kernel_log(3, "setting up global descriptor table");
+	gdt_setup();
+	kernel_log(0, "set up global descriptor table");
 
-	while (1)
-	{
-		if (tty_row > VGA_HEIGHT - 1)
-			tty_row = 0;
-
-		uint8_t scancode = inb(0x60);
-
-		if (!scancode)
-			continue;
-
-		if (last_scancode == scancode)
-			continue;
-
-		if (scancode & 0x80)
-		{
-			last_scancode = 0;
-			continue;
-		}
-
-		if (scancode > CODES_PRESSED_SIZE - 1)
-			continue;
-
-		last_scancode = scancode;
-
-		char key = CODES_PRESSED[scancode];
-
-		if (key == '\b')
-		{
-			if (tty_column == 0)
-			{
-				if (tty_row == 0)
-					continue;
-				tty_row -= 1;
-				tty_column = HISTORY[tty_row];
-			}
-			else
-			{
-				tty_column -= 1;
-				tty_insert_entry(0);
-				HISTORY[tty_row] -= 1;
-				tty_column -= 1;
-			}
-		}
-		else if (key == '\n')
-		{
-			tty_row += 1;
-			tty_column = 0;
-		}
-		else if (key == 0)
-		{
-			continue;
-		}
-		else
-		{
-			HISTORY[tty_row] += 1;
-			tty_insert_entry(key);
-		}
-	}
+	printn("\nslava kokaini");
 }
