3518b9116ed3eabfea50186a32df27d7864339df
senvildev <realsenvil@gmail.com>
2025-03-05 22:37:39 +0100

    added missing comments to strcat function

--- src/kernel/libs/string/string.c
+++ src/kernel/libs/string/string.c
@@ -5,17 +5,18 @@
 // function that connects 2 strings together
 char *strcat(char *destination_text, const char *text_to_add)
 {
+	// create characters to store string sizes
 	size_t i, j;
-
+	// create a buffer with a 1024 character limit
 	static char buffer[1024];
-
+	// for each character in destination text
 	for (i = 0; destination_text[i] != '\0'; i++)
-		buffer[i] = destination_text[i];
-
+		buffer[i] = destination_text[i]; // add character to text buffer
+	// for each character in text to add
 	for (j = 0; text_to_add[j] != '\0'; j++)
-		buffer[i + j] = text_to_add[j];
-	buffer[i + j] = '\0';
-
+		buffer[i + j] = text_to_add[j]; // add character to text buffer
+	buffer[i + j] = '\0';				// end with a null terminator
+	// return the string
 	return buffer;
 };
 
