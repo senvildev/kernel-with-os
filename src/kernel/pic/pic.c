f97431a05b9c36b212242485b48f47d069e37712
senvildev <realsenvil@gmail.com>
2025-03-08 01:12:51 +0100

    added a full implementation of the PIC with comments

---
+++ src/kernel/pic/pic.c
@@ -0,0 +1,112 @@
+#include <stdint.h>
+
+#include "pic/pic.h"
+
+#include "libs/kernel_log/kernel_log.h"
+
+// function to set up the entire PIC
+void pic_setup(void)
+{
+	/* commented since it's not needed in this setup
+	// create variables for interrupt masks
+	uint8_t interrupt_mask1, interrupt_mask2;
+
+	interrupt_mask1 = inb(MASTER_PIC_DATA_PORT);
+	interrupt_mask2 = inb(SLAVE_PIC_DATA_PORT);
+	*/
+
+	// the io_wait(); repetition is required
+	// to ensure that the PIC has enough time
+	// to write data
+
+	kernel_log(3, "setting up the PIC");
+
+	// start initialization sequence
+	// sets bits for ICW1_ICW4 and ICW1_INIT
+	// (ICW1_INIT is required for initialization)
+	// cascade mode
+	// 8 byte interrupt vector interval
+	// edge-triggered mode
+	outb(MASTER_PIC_COMMAND_PORT, ICW1_INIT | ICW1_ICW4);
+	io_wait();
+	outb(SLAVE_PIC_COMMAND_PORT, ICW1_INIT | ICW1_ICW4);
+	io_wait();
+
+	// set vector offsets
+	outb(MASTER_PIC_DATA_PORT, 0x20); // IRQ0 - IRQ7 start at 0x20
+	io_wait();
+	outb(SLAVE_PIC_DATA_PORT, 0x28); // IRQ8 - IRQ15 start at 0x28
+	io_wait();
+
+	// tell the master PIC about the slave PIC
+	// 0b10 says that a slave is connected to IRQ2
+	outb(MASTER_PIC_DATA_PORT, 4);
+	io_wait();
+	// value 2 tells the slave PIC its cascade
+	// identity (that its connected by IRQ2 to
+	// the master)
+	outb(SLAVE_PIC_DATA_PORT, 2);
+	io_wait();
+
+	// set the mode for both PIC's
+	// non auto-EOI
+	// non-buffered mode
+	// normal nested mode
+	outb(MASTER_PIC_DATA_PORT, ICW4_8086);
+	io_wait();
+	outb(SLAVE_PIC_DATA_PORT, ICW4_8086);
+	io_wait();
+
+	// restore masks (and initially mask
+	// all interrupts)
+	// THIS DISABLES ALL INTERRUPTS!!!
+	outb(MASTER_PIC_DATA_PORT, 0xFF);
+	outb(SLAVE_PIC_DATA_PORT, 0xFF);
+
+	kernel_log(0, "set up the PIC and disabled all IRQ's");
+}
+
+// function to enable a specific IRQ
+// in the PIC
+void pic_enable_irq(uint8_t irq)
+{
+	// check just in case the passed
+	// IRQ doesn't match the limit
+	if (irq > 15) // doesn't require a below zero check
+		return;
+
+	// declare required variables
+	uint16_t pic_port; // port of the PIC
+	uint8_t irq_mask;  // mask controlling which IRQ
+					   // is enabled/disabled
+
+	// check whether the IRQ is in the master
+	// PIC or the slave PIC
+	if (irq < 8)
+		//
+		pic_port = MASTER_PIC_DATA_PORT;
+	else
+	{
+		pic_port = SLAVE_PIC_DATA_PORT;
+		irq -= 8;
+	}
+
+	// get the IRQ mask byte
+	irq_mask = inb(pic_port);
+	// clear the IRQ mask (to enable it)
+	irq_mask &= ~(1 << irq_mask);
+
+	// write the IRQ mask to
+	// the PIC port
+	outb(pic_port, irq_mask);
+}
+
+// function that enables all IRQ's
+// in the PIC
+void pic_enable_all_irqs(void)
+{
+	// loop through IRQ0 to IRQ15
+	for (uint8_t i = 0; i < 16; i++)
+		// enable the IRQ
+		pic_enable_irq(i);
+}
---
+++ src/kernel/pic/pic.h
@@ -0,0 +1,144 @@
+#include <stdint.h>
+
+#ifndef PIC_H
+#define PIC_H
+
+// define ports for the PIC
+//	  - intel 8259 PIC chips come in a
+//		master/slave setup
+#define MASTER_PIC_COMMAND_PORT 0x20
+#define MASTER_PIC_DATA_PORT 0x21
+#define SLAVE_PIC_COMMAND_PORT 0xA0
+#define SLAVE_PIC_DATA_PORT 0xA1
+
+// define ICW1 (Initialization Command Word) bits
+//	  - documented inside the Intel
+//		8259A datasheet
+
+// ICW4 follow-up
+//	  - if set to 1	- ICW4 will be sent after ICW3
+//	  - if set to 0 - ICW4 wont be sent and will stop
+//					  on ICW3
+#define ICW1_ICW4 0x01
+
+// single mode
+//	  - if set to 1	- the PIC will operate as a standalone
+//					  controller
+//	  - if set to 0 - the PIC will operate as a part of
+//	  -				  a cascaded configuration (IRQ2 used
+//					  as the connection)
+#define ICW1_SINGLE_MODE 0x02
+
+// call address interval
+//	  - if set to 1 - the interrupt vectors are 8 bits apart
+//					  (for the 8080/8085 architecture)
+//	  - if set to 0 - the interrupt vectors are 4 bits apart
+//					  (for the 8086/8088 architecture)
+#define ICW1_INTERVAL4 0x04
+
+// trigger mode
+//	  - if set to 1 - level-triggered mode; the PIC detects
+//					  the interrupt if its on high voltage
+//	  - if set to 0 - edge-triggered mode; the PIC detects
+//					  the interrupt on the rising edge of
+//					  the signal (when the signal goes from
+//					  low voltage to high voltage)
+#define ICW1_LEVEL 0x08
+
+// initialization mode
+//	  - if set to 1 - signals the PIC that the initialization
+//					  is beginning
+#define ICW1_INIT 0x10
+
+// define ICW4
+
+// processor mode
+//	  - if set to 1 - the PIC operates in 8086 mode
+//					  (suitable for 8086/8088 and x86
+//					  processors)
+//	  - if set to 0 - the PIC operates in MCS-80/85 mode
+//					  (suitable for 8080/8085 processors)
+#define ICW4_8086 0x01
+
+// auto end-of-interrupt
+//	  - if set to 1 - auto-EOI mode; the PIC automatically
+//					  issues an EOI at the end of the interrupt
+//					  acknowledge cycle (no need for a software
+//					  explicit EOI command)
+//	  - if set to 0 - normal EOI mode; the ISR must send an explicit
+//					  signal with an EOI command when it finishes
+//					  processing the interrupt
+#define ICW4_AUTO 0x02
+
+// buffered mode
+//	  - if both set to 0	- non-buffered mode; the PIC directly
+//							  drives the interrupt signals
+//	  - if slave set to 1	- buffered mode as a slave; the PIC's
+//							  output is buffered and it acts as a
+//							  slave
+//	  - if master set to 1	- buffered mode as master; the PIC's
+//							  output is buffered and it acts as a
+//							  master
+#define ICW4_BUFFER_SLAVE 0x08
+#define ICW4_BUFFER_MASTER 0x0C
+
+// special fully nested mode
+//	  - if set to 1 - special fully nested mode; the master PIC
+//					  allows higher priority interrupts from the
+//					  slave PIC, even if it's currently processing
+//					  an interrupt from the slave
+//	  - if set to 0 - normal nested mode; when processing an interrupt
+//					  from the slave PIC, the master won't notice
+//					  higher priority interrupts from the same slave
+//					  until the current one is complete
+#define ICW4_SFNM 0x10
+
+#endif
+
+// function that creates a delay between
+// input/output operations in the PIC
+//	  it writes 0 to port 0x80 using the outb
+//	  instruction. writing to this port takes a
+//	  small amount of time, creating a delay
+static inline void io_wait(void)
+{
+	__asm__ volatile("outb %%al, $0x80" : : "a"(0));
+}
+
+// function that reads a byte from a specified
+// input/output port
+// arguments:
+//	  - port  - 16 bit (0x00) port address
+//				to read from
+static inline uint8_t inb(uint16_t port)
+{
+	uint8_t byte;
+	// read a byte from the specified address and
+	// write it to the variable
+	__asm__ volatile("inb %1, %0" : "=a"(byte) : "Nd"(port));
+	return byte;
+}
+
+// function that writes a byte to a specified
+// input/output port
+// arguments:
+//	  - port  - 16 bit (0x00) port address
+//	  - value - 8 bit (1 byte) value to write
+static inline void outb(uint16_t port, uint8_t value)
+{
+	// write the byte to a specified
+	// 16 bit address
+	__asm__ volatile("outb %0, %1" : : "a"(value), "Nd"(port));
+}
+
+void pic_setup(void);
+
+// function that enables a specific
+// IRQ in the PIC
+// arguments:
+//	  - irq	  - number of the IRQ (0-15)
+void pic_enable_irq(uint8_t irq);
+
+// straightforward function that enables
+// all the IRQ's in the PIC
+void pic_enable_all_irqs(void);
