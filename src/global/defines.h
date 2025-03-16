82263bef5a7b60438845fabf83a948c81aa380f3
senvildev <realsenvil@gmail.com>
2025-03-16 20:36:18 +0100

    added global defines (such as ERROR, SUCCESS, NULL etc.)

---
+++ src/global/defines.h
@@ -0,0 +1,11 @@
+// global level defines
+#define SUCCESS 0
+#define WARNING 2
+#define INFO 3
+
+// kernel-level defines
+#define ERROR 1 // kernel error message
+
+// user level
+#define FAILURE -1		 // function error message
+#define NULL ((void *)0) // null variable
