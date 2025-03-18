0f265ad596ed6ebbf12aae1667c2c7da273b9840
senvildev <realsenvil@gmail.com>
2025-03-18 22:03:44 +0100

    added UI initialization code

--- src/os/os.c
+++ src/os/os.c
@@ -8,9 +8,37 @@
 
 #include "global/defines.h"
 
+#include "libs/ttui/ttui.h"
+
 // initialize the user level space
 void initialize_system(void)
 {
 	tty_clear();
 	print("Hello, Userspace!\n");
+	// initializes a custom text based UI
+	ttui_initialize(VGA_COLOR_WHITE, VGA_COLOR_CYAN);
+	// creates a sidebar
+	// 10 entries wide
+	// empty separator (and connecting separator)
+	// black foreground
+	// dark gray background
+	ttui_create_sidebar(10, ' ', ' ', VGA_COLOR_BLACK, VGA_COLOR_DARK_GRAY);
+	// creates a title bar
+	// contains the specified text
+	// empty separator
+	// 3 rows high
+	// black foreground
+	// light gray background
+	ttui_create_title(
+		"Hallo Welt! Hello World! Hola Mundo!",
+		' ',
+		3,
+		VGA_COLOR_BLACK,
+		VGA_COLOR_LIGHT_GRAY);
+	// entries to be created in the main block
+	ttui_write_to_main_block(
+		"hello guys, this is my first operating system and kernel and this "
+		"took me ~1 month and 1000 lines of code");
+	ttui_write_to_main_block("i wrote this myself");
+	ttui_write_to_main_block("this is my best project so far");
 }
